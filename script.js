9cce800f05c30e901507cbaa4fbd71fed76b0f40
Hima188 <ibrahimahmed188@proton.me>
2025-09-14 13:43:29 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,109 @@
+//  $(document).ready(function(){
+//     // everything inside it 
+//  })
+
+// ----------------------------------------
+ //  selector
+$("p") // by elemet
+$(".ele") // by class
+$("#demo") // by id
+// -----------------------------------------
+// event on button
+
+// Hide
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $('section').hide(10000)
+//     })
+// })
+// $(document).ready(function(){
+//     $(".start").click(function(){
+//         $('section').hide(1000,function(){
+//             console.log("Hiding is done")
+//         })
+//     })
+// })
+
+// Show
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $('section').show();
+//     })
+// })
+
+// Toggle
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("section").toggle();
+//     })
+// })
+
+// // fadeout
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("section").fadeOut();
+//     })
+// })
+
+// // fade in
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("section").fadeIn();
+//     })
+// })
+
+// Fade Toggle
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("section").fadeToggle();
+//     })
+// })
+
+// Slide down
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("section").slideDown();
+//     })
+// })
+
+// Slide up
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("section").slideUp();
+//     })
+// })
+
+// SlideToggle
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("section").slideToggle();
+//     })
+// })
+//Animate
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("section").animate({
+//             left: "100px"
+//         })
+//     })
+// }) 
+
+//Stop
+// $(document).ready(function(){
+//     $(".start").click(function(){
+//         $("section").slideToggle(3000);
+//     });
+//     $(".stop").click(function(){
+//         $("section").stop();
+//     });
+// });
+
+// chain
+// $(document).ready(function(){
+//     $(".start").click(function(){
+//         $("section").hide(1000).show(1000).slideUp(1000);
+//     })
+// })
+// ------------------------------------------
+
+// Dom manpulation
